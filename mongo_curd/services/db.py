4669cb89a4d0275f979695ebdf4ca5cd6ce4fa82
skmuneer <muneer@nuevesolutions.com>
2022-01-07 18:05:11 +0530

    dynamic envs ,some of fields optional

--- mongo_curd/services/db.py
+++ mongo_curd/services/db.py
@@ -4,8 +4,11 @@ import os
 
 username = os.environ['M_USERNAME']
 pwd = os.environ['M_PASSWORD']
+
+print("username",username+" "+pwd)
+
 client = MongoClient(
-    "mongodb+srv://{username}:{pwd}@cluster0.xwnk6.mongodb.net/ffrooms?retryWrites=true&w=majority"
+   "mongodb+srv://"+username+":"+pwd+"@cluster0.xwnk6.mongodb.net/ffrooms?retryWrites=true&w=majority"
 )
 db = client.ffrooms  # db selecting database
 # post = {"author": "Mike",
@@ -13,7 +16,7 @@ db = client.ffrooms  # db selecting database
 #         "tags": ["mongodb", "python", "pymongo"],
 #         "date": datetime.datetime.utcnow()}
 
-mydb = db.users  #connecting to collection
+mydb = db.ffrooms  #connecting to collection
 # ffroom_id=ffrooms.insert_one(post).inserted_id
 # print(ffroom_id)
 # print(db)
